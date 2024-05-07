2cb3d9aa90ddca6e889d59f41bb5ea09d32a4f7f
AlexandrKobelchak <kobelchak@gmail.com>
2024-05-07 19:34:59 +0300

    add application

---
+++ src/main/java/org/example/JkobelchakApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkobelchakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JkobelchakApplication.class, args);
+    }
+
+}
